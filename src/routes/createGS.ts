f10d5445c95e85a77042fc6e32109bd2f9ff6ab4
Tiew <tnttiew@gmail.com>
2025-11-16 11:59:36 +1100

    stuff

---
+++ src/routes/createGS.ts
@@ -0,0 +1,27 @@
+function cap(s: String) {
+    return s.charAt(0).toUpperCase() +
+            s.slice(1)
+}
+
+class LangVar {
+    type: String
+    name: String
+    constructor(type: String, name: String) {
+        this.type = type
+        this.name = name
+    }
+}
+
+class Cpp extends LangVar {
+    constructor(type: String, name: String) {
+        super(type, name)
+    }
+
+    getter() {
+        return `
+${this.type} get${cap(this.name)}() {
+        return ${this.name};
+}  
+        `
+    }
+}
